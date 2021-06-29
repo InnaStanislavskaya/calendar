cae3e0153c7d1a1cd15ed7bb70dda79a8d76f422
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2021-06-29 21:16:32 +0300

    hw38_done

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import {useState} from 'react';
+import { Year } from '../components/Year';
+import './App.css';
+
+export function App() {
+  const [currentYear, setYear] = useState(new Date().getFullYear());
+
+  const handleYearChange = (newValue) => {
+    setYear(newValue);
+  }
+  
+  return (
+    <div className="App">
+      <Year year ={currentYear} onYearChange={handleYearChange}></Year>
+    </div>
+  );
+}
+
+
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export * from '../App/App';
+export * from './Year';
+export * from './Day';
+export * from './Month';
